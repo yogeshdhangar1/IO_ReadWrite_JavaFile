bc3cef5bb5f92be777306e312b935697b563f9fd
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-10 00:51:38 -0800

    Added:Create Watch Service

---
+++ src/test/java/TestCasesForJavaIO.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestCasesForJavaIO {
+    @Test
+    public void givenPathWhenCheckedThenConfirm() {
+        Path homePath = Paths.get("C:\\intallije workplace\\File_IO_WatchService\\PayRoll.txt");
+        Assert.assertTrue(Files.exists(homePath));
+    }
+    @Test
+    public void givenFileCheckedAreAvailable_NotIfAvailableThenDelete() throws IOException, IOException {
+        Path playPath=Paths.get("C:\\IntelliJ workplace\\File_IO_WatchService\\"+"/"+"PayRoll.txt");
+        Files.deleteIfExists(Paths.get("C:\\IntelliJ workplace\\File_IO_WatchService\\PayRoll.txt"));
+        Assert.assertTrue(Files.notExists(playPath));
+    }
+    
+}
+
