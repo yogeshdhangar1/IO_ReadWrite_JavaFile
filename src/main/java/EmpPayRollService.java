eeaadac2b5ee92a3157e86111df9c18bfde5eaa3
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-10 03:09:32 -0800

    Added:Ability To Check Entries

--- src/main/java/EmpPayRollService.java
+++ src/main/java/EmpPayRollService.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
@@ -8,25 +9,12 @@ import java.util.Scanner;
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class EmpPayRollService {
-    public void listFilesDirectory() throws IOException, InterruptedException  {
-        ArrayList<String> writing = new ArrayList<String>();
+    public long listFilesDirectory() throws IOException, InterruptedException  {
 
-        writing.add("Id : 111");
-        writing.add("Name : Yogesh");
-        writing.add("Salary : 150000");
-//		File Exist => Override
-        // Will create new file if not exist
-        Files.write(Paths.get("C:\\intallije workplace\\File_IO_WatchService\\Test1.txt"), writing, StandardOpenOption.CREATE);
-        System.out.println("The Writing is done.......");
-
-        List<String> line=	Files.readAllLines(Paths.get("C:\\intallije workplace\\File_IO_WatchService\\Test1.txt"));
-        for(String lines:line) {
-            System.out.println(lines);
-        }
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        EmpPayRollService obj = new EmpPayRollService();
-        obj.listFilesDirectory();
+        long enteries=0;
+        enteries=Files.lines(new File("C:\\intallije workplace\\File_IO_WatchService\\Test1.txt").toPath())
+                .count();
+        System.out.println(enteries);
+        return enteries;
     }
 }
